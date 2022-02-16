366aad0638a30d3932120fdc7f39b0b5c88e12a7
JunHyeongB <aowlr0513@gmail.com>
2022-02-16 17:58:16 +0900

    feat: Game 컴포넌트 추가

---
+++ components/games/Game.tsx
@@ -0,0 +1,40 @@
+import classes from "./Game.module.scss";
+
+export type GameProps = {
+  game: {
+    developer: string;
+    freetogame_profile_url: string;
+    game_url: string;
+    genre: string;
+    id: number;
+    platform: string;
+    publisher: string;
+    release_date: string;
+    short_description: string;
+    thumbnail: string;
+    title: string;
+  };
+};
+
+const Game = (props: GameProps): JSX.Element => {
+  return (
+    <div className={classes.container}>
+      <img src={props.game.thumbnail} alt={props.game.title} />
+      <div className={classes.game}>
+        <h2>
+          {props.game.title}{" "}
+          <span className={classes.publisher}>{props.game.publisher}</span>
+        </h2>
+
+        <div className={classes.badge_container}>
+          <span className={classes.badge}>{props.game.platform}</span>
+          <span className={classes.badge}>{props.game.release_date}</span>
+        </div>
+
+        <p>{props.game.short_description}</p>
+      </div>
+    </div>
+  );
+};
+
+export default Game;
---
+++ components/games/index.ts
@@ -0,0 +1,2 @@
+export { default as Game } from "./Game";
+export type { GameProps } from "./Game";
