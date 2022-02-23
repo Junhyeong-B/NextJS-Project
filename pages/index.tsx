b892321be0d813423461907e4a3837858cf09a60
JunHyeongB <aowlr0513@gmail.com>
2022-02-23 18:55:14 +0900

    feat: index page에 routing 추가

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,20 +1,33 @@
 import { NextPage } from "next";
+import { useRouter } from "next/router";
 import { customAxios } from "../apis";
 import { Game, GameProps } from "../components";
+import classes from "../styles/home.module.scss";
 
 type HomePageProps = {
   games: GameProps[];
 };
 
 const HomePage: NextPage<HomePageProps> = (props) => {
+  const router = useRouter();
+  const gameClickHandler = (id: number) => {
+    router.push("/" + id);
+  };
+
   return (
-    <div>
-      <ul>
-        {props.games.map((game) => (
-          <li key={game.id}>
-            <Game game={game} />
-          </li>
-        ))}
+    <div className={classes.container}>
+      <h1 className={classes.h1}>💻PC Live games list</h1>
+      <ul className={classes.game_list}>
+        {props.games.map((game, i) => {
+          if (i > 5) {
+            return;
+          }
+          return (
+            <li onClick={() => gameClickHandler(game.id)} key={game.id}>
+              <Game game={game} />
+            </li>
+          );
+        })}
       </ul>
     </div>
   );
@@ -24,7 +37,9 @@ export default HomePage;
 
 export async function getServerSideProps() {
   const response = await customAxios.get("/api/games");
-  const result = response.data.filter((_: GameProps, i: number) => i < 20);
+  const result = await response.data.filter(
+    (_: GameProps, i: number) => i < 20
+  );
 
   return {
     props: {
